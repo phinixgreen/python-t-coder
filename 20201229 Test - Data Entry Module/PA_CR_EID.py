eeb719543a1ea64b820cbe1ee08b794b701c43f5
Muhammad Mustafa Monowar <31995212+phinixgreen@users.noreply.github.com>
2020-12-29 21:12:54 +0600

    Test Data Entry
    - Success
    - Clicking the PA_CR_EID will create a data entry form where the user will be asked to enter an EID, and Last Name

---
+++ 20201229 Test - Data Entry Module/PA_CR_EID.py
@@ -0,0 +1,6 @@
+PA_CR_EID = open("PA_DB_EID.csv","a")     ## define PA_CR_EID as open text.txt and append
+EID = input ("Enter the EID: ")   ## define val as input
+lastName = input ("Enter the Last Name of the Employee: ") 
+PA_CR_EID.write("\n" + EID + ","+ lastName)          ## write to PA_CR_EID including input received from val  
+PA_CR_EID.close()                ## close PA_CR_EID
+
