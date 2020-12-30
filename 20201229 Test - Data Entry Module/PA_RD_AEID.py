c44d539f2d2c97f1614e1cbf6d4a4e04307c7954
phinixgreen <mustafamonowar.esl@gmail.com>
2020-12-31 00:04:44 +0600

    pandas read dataframe successful
    Data Entry Module now support viewing the whole dataframe

---
+++ 20201229 Test - Data Entry Module/PA_RD_AEID.py
@@ -0,0 +1,4 @@
+import pandas as pd   ## imports pandas as pd
+PA_DB_AEID = pd.read_csv('PA_DB_EID.csv')  ## Creates PA_DB_AEID and reads CSV Data file
+print (PA_DB_AEID)    ## Prints all data
+
--- test/PA_RD_AEID.py
+++ test/PA_RD_AEID.py
@@ -1,4 +1,4 @@
 import pandas as pd
 PA_DB_EID = pd.read_csv('PA_DB_EID.csv')
-print (PA_DB_EID.head)
+print (PA_DB_EID)
 
