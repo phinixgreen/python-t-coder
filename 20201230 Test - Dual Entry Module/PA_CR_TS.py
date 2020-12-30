d06eabf5bb099194a07e169d3c1c04d9ad5c0032
phinixgreen <mustafamonowar.esl@gmail.com>
2020-12-31 00:55:11 +0600

    comments updated for dual entry module

--- 20201230 Test - Dual Entry Module/PA_CR_TS.py
+++ 20201230 Test - Dual Entry Module/PA_CR_TS.py
@@ -1,20 +1,17 @@
-import time;
-ts = time.time()
+import time;             # import time
+ts = time.time()         # define ts as time.time()
 
-PA_CR_LN = open("PA_DB_LN.csv","a")     ## define PA_CR_EID as open text.txt and append
-     ## define PA_CR_EID as open text.txt and append
+PA_CR_LN = open("PA_DB_LN.csv","a")     ## define PA_CR_LN as PA_CR_LN.csv and append
 
-lastName = input ("Enter the Last Name of the Employee: ") 
+lastName = input ("Enter the Last Name of the Employee: ")  ## enters the last name
 
-PA_CR_LN.write("\n" + str(ts) + "," + lastName)          ## write to PA_CR_EID including input received from val  
-PA_CR_LN.close()
+PA_CR_LN.write("\n" + str(ts) + "," + lastName)          ## write to PA_CR_LN with lastname and ts  
+PA_CR_LN.close()                                         ## PA_CR_LN close   
 
-PA_CR_FN = open("PA_DB_FN.csv","a")     ## define PA_CR_EID as open text.txt and append
-     ## define PA_CR_EID as open text.txt and append
+PA_CR_FN = open("PA_DB_FN.csv","a")     ## define PA_CR_FN as PA_CR_FN.csv and append
 
-firstName = input ("Enter the First Name of the Employee: ") 
+firstName = input ("Enter the First Name of the Employee: ") ## enters the first name
 
-PA_CR_FN.write("\n" + str(ts) + "," + firstName)          ## write to PA_CR_EID including input received from val  
-PA_CR_FN.close()
+PA_CR_FN.write("\n" + str(ts) + "," + firstName)          ## write to PA_CR_FN with firstname and ts 
 
-print("Command Successful")
+print("Command Successful")                               ## prints command successful
